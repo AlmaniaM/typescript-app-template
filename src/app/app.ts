d9712ae370b68c2b77c9672864181c8157b0d159
AlmaniaM <8919797+AlmaniaM@users.noreply.github.com>
2020-10-27 21:49:15 -0700

    Removed unused types

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,3 @@
-import {EmailSenderConfig, MainConfig, MainConfigProjectInfo} from './types';
-
-export interface MainParams {
-  projectName: string;
-  testName: string;
-  isProduction: boolean;
-  mainConfigPath: string;
-  rootDirectoryPath: string;
-}
-
 export class App {
   public async run(params: string[], cwd: string): Promise<void> {
     console.log('Hello World');
